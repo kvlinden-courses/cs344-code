b7a8918d58db0551696dc8f52ffe2cf49b021c4f
kvlinden <kvlinden@calvin.edu>
2020-02-14 17:44:57 -0500

    prepped unit 3...

--- u03constraint/queens.py
+++ u03constraint/queens.py
@@ -12,15 +12,15 @@ from search import depth_first_graph_search
 import logging
 
 # 1. Set up the problem.
-n = 4
+n = 8
 problem = NQueensCSP(n)
 
 # 2. Solve the problem.
 # There is a bug in the DFS code (even for 1-queens), so skip this one.
-solution = depth_first_graph_search(problem)
+# solution = depth_first_graph_search(problem)
 # solution = AC3(problem);
 # solution = backtracking_search(problem)
-# solution = min_conflicts(problem)
+solution = min_conflicts(problem)
 
 # 3. Print the results.  
 # Handle AC3 solutions (boolean values) first, they behave differently.
