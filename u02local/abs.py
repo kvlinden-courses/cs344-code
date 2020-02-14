d3c31f2f14e81d7cc497548067a1d30d1d77a5d3
kvlinden <kvlinden@calvin.edu>
2020-02-14 14:15:12 -0500

    added timing functions to lab 2...

--- u02local/abs.py
+++ u02local/abs.py
@@ -6,10 +6,11 @@ The function is a linear function  with a single, discontinuous max value
 @author: kvlinden
 @version 6feb2013
 """
+import math
+import time
 from search import Problem, hill_climbing, simulated_annealing, \
     exp_schedule, genetic_search
 from random import randrange
-import math
 
 
 class AbsVariant(Problem):
@@ -38,22 +39,31 @@ if __name__ == '__main__':
     # Formulate a problem with a 2D hill function and a single maximum value.
     maximum = 30
     initial = randrange(0, maximum)
+    time1 = time.time()
     p = AbsVariant(initial, maximum, delta=1.0)
+    time2 = time.time()
     print('Initial                      x: ' + str(p.initial)
           + '\t\tvalue: ' + str(p.value(initial))
+          + "\t\ttime: %0.3f seconds" % (time2 - time1)
           )
 
     # Solve the problem using hill-climbing.
+    time1 = time.time()
     hill_solution = hill_climbing(p)
+    time2 = time.time()
     print('Hill-climbing solution       x: ' + str(hill_solution)
           + '\tvalue: ' + str(p.value(hill_solution))
+          + "\t\ttime: %0.3f seconds" % (time2 - time1)
           )
 
     # Solve the problem using simulated annealing.
+    time1 = time.time()
     annealing_solution = simulated_annealing(
         p,
         exp_schedule(k=20, lam=0.005, limit=1000)
     )
+    time2 = time.time()
     print('Simulated annealing solution x: ' + str(annealing_solution)
           + '\tvalue: ' + str(p.value(annealing_solution))
+          + "\t\ttime: %0.3f seconds" % (time2 - time1)
           )
